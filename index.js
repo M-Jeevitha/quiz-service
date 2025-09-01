3e35bd8dc8226e5e47d08278eae9ed36f0832bce
M-Jeevitha <jeevithamuthu2110@gmail.com>
2025-09-01 15:17:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.get('/health', (req, res) => res.json({status: 'ok', service: 'student-service'}));
-app.get('/students', (req, res) => res.json([{id:1, name:'Gokul'}]));
-app.listen(process.env.PORT||3001, ()=> console.log('student-service running'));
+app.get('/health', (req, res) => res.json({status: 'ok', service: 'quiz-service'}));
+app.listen(process.env.PORT||3002, ()=> console.log('quiz-service running'));
